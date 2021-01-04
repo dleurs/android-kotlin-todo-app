9f919e5623bd510b2776bad6b11ca75ea1831ede
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-01-04 12:24:12 +0100

    Start to build the list

--- app/src/main/java/fr/dleurs/android/todoapp/MainActivity.kt
+++ app/src/main/java/fr/dleurs/android/todoapp/MainActivity.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val listTodoUi = findViewById<TextView>(R.id.list_todo)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var listTodo:MutableList<Todo> = mutableListOf<Todo>()
+        listTodo.add(Todo("Do sport",false))
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -21,6 +28,10 @@ class MainActivity : AppCompatActivity() {
         when(item.itemId) {
             R.id.menu_add_button -> {
                 Toast.makeText(this, "Adding a todo", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.menu_delete_button -> {
+                Toast.makeText(this, "Deleting a todo", Toast.LENGTH_SHORT).show()
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
---
+++ app/src/main/java/fr/dleurs/android/todoapp/Todo.kt
@@ -0,0 +1,3 @@
+package fr.dleurs.android.todoapp
+
+class Todo(var name: String, var done:Boolean);
