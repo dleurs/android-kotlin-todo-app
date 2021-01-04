b192e5ec7aacf513f637f70263fbcd37ae109350
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-01-04 11:26:52 +0100

    OptionBar added

--- app/src/main/java/fr/dleurs/android/todoapp/MainActivity.kt
+++ app/src/main/java/fr/dleurs/android/todoapp/MainActivity.kt
@@ -2,10 +2,28 @@ package fr.dleurs.android.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_add_button -> {
+                Toast.makeText(this, "Adding a todo", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
