ca7b7770ee39a70924e73ce0cd0bc1fa6e36f810
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-02-03 18:18:52 +0100

    Not yet link UI with API

---
+++ app/src/main/java/com/example/android/roomwordssample/mocki/TodoApiService.kt
@@ -0,0 +1,36 @@
+package com.example.android.roomwordssample.mocki
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+/**
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface TodoApiService {
+    @GET("todo")
+    suspend fun getTodos(): List<TodoProperty>
+}
+
+object TodoApi {
+    val retrofitService : TodoApiService by lazy { retrofit.create(TodoApiService::class.java) }
+}
---
+++ app/src/main/java/com/example/android/roomwordssample/mocki/TodoProperty.kt
@@ -0,0 +1,18 @@
+package com.example.android.roomwordssample.mocki
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * This data class defines a Mars property which includes an ID, the image URL, the type (sale
+ * or rental) and the price (monthly if it's a rental).
+ * The property names of this data class are used by Moshi to match the names of values in JSON.
+ */
+@Parcelize
+data class TodoProperty (
+    val userId: String,
+    val id: String,
+    val title: String,
+    val completed: Boolean)
+
